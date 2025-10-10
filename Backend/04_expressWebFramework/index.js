048a470ddd825cf1abf3cea0f8e1fa12a6fdadf0
gaurav0973 <codejourneypanda@gmail.com>
2025-10-10 21:49:53 +0530

    again vahi error aa gaya

--- Backend/04_expressWebFramework/index.js
+++ Backend/04_expressWebFramework/index.js
@@ -2,23 +2,23 @@ import express from "express"
 const app = express()
 
 
-app.use(express.json())
+// app.use(express.json())
 
-app.get("/user", (req, res)=>{
-    console.log("THis is res : ", res.body)
-    console.log("THis is params in req : ", req.params)
-    res.send({
-        user : "Gaurav",
-        age : 22
-    })
-})
+// app.get("/user", (req, res)=>{
+//     console.log("THis is res : ", res.body)
+//     console.log("THis is params in req : ", req.params)
+//     res.send({
+//         user : "Gaurav",
+//         age : 22
+//     })
+// })
 
-app.post("/user", (req, res)=>{
-    console.log(req.body)
-    res.send({
-        message : "Responce body is being send"
-    })
-})
+// app.post("/user", (req, res)=>{
+//     console.log(req.body)
+//     res.send({
+//         message : "Responce body is being send"
+//     })
+// })
 
 
 app.listen(8888, ()=>{
