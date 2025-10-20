4d0bad12eec27781c648ea6e8e96bd51ed7d2181
gaurav0973 <codejourneypanda@gmail.com>
2025-10-20 17:00:08 +0530

    event and event-bubbling

--- 04_DOM/03_eventListner/script.js
+++ 04_DOM/03_eventListner/script.js
@@ -1,11 +1,8 @@
-
-
 // METHOD-1 : onClick in html tags
 // function handleClick(){
 //     const element = document.getElementById("heading")
 //     element.textContent = "Apna badhiya, aap batao"
 // }
-
 
 // METHOD-2 : This will override if written another time
 // const element = document.getElementById("heading")
@@ -15,8 +12,6 @@
 // element.onclick = function(){
 //     element.textContent = "Chupp"
 // }
-
-
 
 // METHOD-3 : Best Method
 // const element = document.getElementById("heading")
@@ -28,3 +23,21 @@
 // element.addEventListener("click", ()=>{
 //     element.style.background = "brown"
 // })
+
+
+
+const grandparent = document.getElementById("grandparent")
+const parent = document.getElementById("parent")
+const child = document.getElementById("child")
+
+grandparent.addEventListener("click", () => {
+  console.log("Grandparent hu mai");
+}, true)
+
+parent.addEventListener("click", () => {
+  console.log("Parent hu mai");
+}, true)
+
+child.addEventListener("click", () => {
+  console.log("Child (Target)");
+}, true)
