5398bf6b80cecfbea05996146e4b05c956fb0a02
gaurav0973 <codejourneypanda@gmail.com>
2025-10-24 02:49:48 +0530

    started task-board web app

---
+++ 07_DOM_MAJOR_PROJECTS/01_Kanban_board/script.js
@@ -0,0 +1,50 @@
+let draggingElement = null
+
+function addTask(columnId){
+    const input = document.getElementById(`${columnId}-input`)
+    // console.log(input)
+    const inputValue = input.value.trim()
+    if(inputValue.length === 0)
+        return
+
+    const taskElement = createTaskElement(inputValue)
+
+    const parent = document.getElementById(`${columnId}-tasks`)
+    parent.append(taskElement)
+
+    input.value = ""
+
+}
+
+function createTaskElement(inputValue){
+    const div = document.createElement("div")
+    div.textContent = inputValue
+    div.classList.add("card")
+    // drag and drop functionality
+    div.draggable=true
+    div.addEventListener("dragstart", dragStart)
+    div.addEventListener("dragend", dragEnd)
+    return div
+}
+
+
+function dragStart(){
+    // this.classList.add("dragging")
+    draggingElement = this
+}
+
+function dragEnd(){
+    // this.classList.remove("dragging")
+    draggingElement = null
+}
+
+
+const columns = document.querySelectorAll(".tasks")
+columns.forEach((column)=>{
+    column.addEventListener("dragover", dragOver)
+})
+
+function dragOver(e){
+    e.preventDefault()
+    this.append(draggingElement)
+}
